4f453509fee596f6b19354675ce8a40f6c989bee
nehayd <89801073+nehayd@users.noreply.github.com>
2021-10-11 18:29:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Write your code below this line 👇
 name = str(input("Enter your name.\n"))
-print("The number og characters in the username is %s" %len(name))
+print("The number of characters in the username is %s" %len(name))
 
 
 
